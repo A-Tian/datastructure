499074e5e281c103f87816b2007ac493d52fc86d
Shu-Tian Lin <atian1129@gmail.com>
2016-04-30 14:28:38 +0800

    add comment and Screenshot

--- Q1/main.py
+++ Q1/main.py
@@ -2,27 +2,27 @@ from StackModule import Stack
 
 def infixToPrefix(infixexpr):
     prec = {}
-    prec["*"] = 3
+    prec["*"] = 3	#設定優先順序
     prec["/"] = 3
     prec["+"] = 2
     prec["-"] = 2
-    prec[")"] = 1
+    prec[")"] = 1	#設定優先順序
     opStack = Stack()
     prefixList = []
-    tokenList = infixexpr.split()
-    tokenList.reverse()
+    tokenList = infixexpr.split()	#將字串分割
+    tokenList.reverse()	#反轉排序
 
     for token in tokenList:
-        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":	#如果是英文或數字直接新增
             prefixList.append(token)
-        elif token == ')':
+        elif token == ')':	#判斷op
             opStack.push(token)
-        elif token == '(':
+        elif token == '(':	#判斷op
             topToken = opStack.pop()
             while topToken != ')':
                 prefixList.append(topToken)
                 topToken = opStack.pop()
-        else:
+        else:	#判斷op
             while (not opStack.isEmpty()) and \
                (prec[opStack.peek()] > prec[token]):
                   prefixList.append(opStack.pop())
@@ -30,7 +30,7 @@ def infixToPrefix(infixexpr):
 
     while not opStack.isEmpty():
         prefixList.append(opStack.pop())
-    prefixList.reverse()
+    prefixList.reverse()	#反轉排序
     return " ".join(prefixList)
 
 print(infixToPrefix("A + B"))
--- Q2/QueueModule.py
+++ Q2/QueueModule.py
@@ -5,10 +5,10 @@ class Queue:
     def isEmpty(self):
         return self.items == []
 
-    def enqueue(self, item):
+    def enqueue(self, item):    #從最左邊加入
         self.items.insert(0, item)
 
-    def dequeue(self):
+    def dequeue(self):  #從最右邊彈出
         return self.items.pop()
 
     def size(self):
